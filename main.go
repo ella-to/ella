59989fee0f9065c7dcd7ef6d43e193d83a8fd05b
Ali Najafizadeh <a.najafizadeh@gmail.com>
2025-03-07 23:47:15 -0500

    Support only one level of glob

--- main.go
+++ main.go
@@ -69,24 +69,26 @@ func main() {
 	}
 }
 
-func format(path string) error {
-	filenames, err := filepath.Glob(path)
-	if err != nil {
-		return err
-	}
-
-	for _, filename := range filenames {
-		doc, err := ParseDocument(NewParserWithFilenames(filename))
+func format(searchPaths ...string) error {
+	for _, searchPath := range searchPaths {
+		filenames, err := filesFromGlob(searchPath)
 		if err != nil {
 			return err
 		}
 
-		var sb strings.Builder
-		doc.Format(&sb)
+		for _, filename := range filenames {
+			doc, err := ParseDocument(NewParserWithFilenames(filename))
+			if err != nil {
+				return err
+			}
 
-		err = os.WriteFile(filename, []byte(sb.String()), os.ModePerm)
-		if err != nil {
-			return err
+			var sb strings.Builder
+			doc.Format(&sb)
+
+			err = os.WriteFile(filename, []byte(sb.String()), os.ModePerm)
+			if err != nil {
+				return err
+			}
 		}
 	}
 
@@ -97,7 +99,7 @@ func gen(pkg, out string, searchPaths ...string) (err error) {
 	var docs []*Document
 
 	for _, searchPath := range searchPaths {
-		filenames, err := filepath.Glob(searchPath)
+		filenames, err := filesFromGlob(searchPath)
 		if err != nil {
 			return err
 		}
@@ -117,4 +119,51 @@ func gen(pkg, out string, searchPaths ...string) (err error) {
 	}
 
 	return Generate(pkg, out, docs)
+}
+
+// make sure only pattern is used at the end of the search path
+// and only one level of search path is allowed
+func filesFromGlob(searchPath string) ([]string, error) {
+	filenames := []string{}
+
+	dir, pattern := filepath.Split(searchPath)
+	if dir == "" {
+		dir = "."
+	}
+
+	if strings.Contains(dir, "*") {
+		return nil, fmt.Errorf("glob pattern should not be used in dir level: %s", searchPath)
+	}
+
+	fmt.Println("dir: ", dir, "pattern: ", pattern)
+
+	entries, err := os.ReadDir(dir)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, entry := range entries {
+		fmt.Println(entry.Name(), entry.IsDir())
+	}
+
+	return filenames, nil
+
+	for _, entry := range entries {
+		if entry.IsDir() {
+			continue
+		}
+		match, err := filepath.Match(pattern, entry.Name())
+		if err != nil {
+			return nil, err
+		}
+		if match {
+			filenames = append(filenames, filepath.Join(dir, entry.Name()))
+		}
+	}
+
+	for _, filename := range filenames {
+		fmt.Println(filename)
+	}
+
+	return filenames, nil
 }
